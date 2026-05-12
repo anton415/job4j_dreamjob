32cf6888d1668f805eb206da3602dfd6cc3479f8
Anton Serdyuchenko <anton415@gmail.com>
2026-05-12 16:54:37 +0300

    7. Создание, просмотр, редактирование и удаление кандидатов [#504858]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import ru.job4j.dreamjob.repository.CandidateRepository;
 import ru.job4j.dreamjob.repository.MemoryCandidateRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.job4j.dreamjob.model.Candidate;
 
 @Controller
 @RequestMapping("/candidates")
@@ -22,5 +26,36 @@ public class CandidateController {
     @GetMapping("/create")
     public String getCreationPage() {
         return "candidates/create";
+    }
+
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        var candidateOptional = candidateRepository.findById(id);
+        if (candidateOptional.isEmpty()) {
+            model.addAttribute("message", "Кандидат с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        model.addAttribute("candidate", candidateOptional.get());
+        return "candidates/one";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Candidate candidate, Model model) {
+        var isUpdated = candidateRepository.update(candidate);
+        if (!isUpdated) {
+            model.addAttribute("message", "Кандидат с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        var isDeleted = candidateRepository.deleteById(id);
+        if (!isDeleted) {
+            model.addAttribute("message", "Кандидат с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
     }
 }
